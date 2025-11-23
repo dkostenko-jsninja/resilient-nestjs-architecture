4e92025cb11a104ad17ae7eaddca16b1698989d5
Dmytro Kostenko <dmytro.kostenko@targomo.com>
2025-11-23 12:24:18 +0100

    feat(infrastructure): parametrize Postgres max connections

--- src/configs/postgres/datasource.ts
+++ src/configs/postgres/datasource.ts
@@ -4,10 +4,32 @@ import { join } from 'path'
 import { DataSource } from 'typeorm'
 import { getValidatedConfig } from '../validation'
 
+interface PostgresConfig {
+  url: string
+  maxConnections: number
+}
+
+const rawConfig: Record<keyof PostgresConfig, string | undefined> = {
+  url: process.env.POSTGRES_URL,
+  maxConnections: process.env.POSTGRES_MAX_CONNECTIONS,
+}
+
+const config = getValidatedConfig<PostgresConfig>(
+  'POSTGRES_CONFIG',
+  rawConfig,
+  Joi.object({
+    url: Joi.string().uri().required(),
+    maxConnections: Joi.number().integer().min(1).default(10),
+  }),
+)
+
 export default new DataSource({
   type: 'postgres',
-  url: getValidatedConfig('POSTGRES_CONFIG_KEY', process.env.POSTGRES_URL ?? '', Joi.string().uri().required()),
+  url: config.url,
   entities: [join(__dirname, '../../', '**/modules/**/infrastructure/db/postgres/*.entity.{js,ts}')],
   migrations: [join(__dirname, 'migrations/*.{js,ts}')],
   synchronize: false,
+  extra: {
+    max: config.maxConnections,
+  },
 })
